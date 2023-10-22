a5fbd9cfbda6c174e56505b9bc43843ea482bb12
returnT0 <vlvolkov25@gmai.com>
2023-10-22 19:41:54 +0200

    sidebar component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthenticationService} from "../authentication.service";
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  public sidebarLinks = [
+    {route: '/home', label: 'Home', imgURL: '/assets/assets/home.svg'},
+    {route: '/user', label: 'User', imgURL: '/assets/assets/user.svg'},
+    {route: '/card', label: 'Card', imgURL: '/assets/assets/store.svg'},
+  ];
 
+  constructor(private authService: AuthenticationService) {
+  }
+
+  get isSignedIn(): boolean {
+    return this.authService.isUserSignedIn();
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  signIn(): void {
+    this.authService.signIn();
+  }
+
+  public isActive(linkRoute: string, currentPath: string): boolean {
+    return (currentPath.includes(linkRoute) && linkRoute.length > 1) || currentPath === linkRoute;
+  }
 }
