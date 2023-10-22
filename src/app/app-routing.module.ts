6e242019938a053ae227a75a40c19ca0c76c1900
returnT0 <vlvolkov25@gmai.com>
2023-10-22 17:13:24 +0200

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
-
+import {HomeComponent} from "./home/home.component";
+import {SignInComponent} from "./auth/sign-in/sign-in.component";
+import {SignUpComponent} from "./auth/sign-up/sign-up.component";
+import {LandingComponent} from "./landing/landing.component";
+import {UserComponent} from "./user/user.component";
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/landing', pathMatch: 'full'},
-
+  {path: 'landing', component: LandingComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   imports: [
@@ -18,6 +25,13 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   declarations: [
     AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    LandingComponent,
+    NavComponent,
+    SidebarComponent,
+    UserComponent,
   ],
   bootstrap: [AppComponent]
 })
