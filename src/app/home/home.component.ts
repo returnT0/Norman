28ab4470a21f4c71acfb1392e594ebe1b05e94d1
returnT0 <vlvolkov25@gmai.com>
2023-10-22 17:28:58 +0200

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthenticationService} from "../authentication.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  showLoading: boolean = true;
+  showGallery: boolean = false;
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.showLoading = false;
+      this.showGallery = true;
+    }, 1000);
+  }
+
+  constructor(private authService: AuthenticationService) {}
+
+  get isSignedIn(): boolean {
+    return this.authService.isUserSignedIn();
+  }
+  signOut(): void {
+    this.authService.signOut();
+  }
+  signIn(): void {
+    this.authService.signIn();
+  }
 }
